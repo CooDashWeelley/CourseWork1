4698c38535f89b8cbeaf8f7181401c3e9a320fbd
CooDashWeelley <iwan.kudashow@gmail.com>
2025-07-21 00:18:39 +0300

    edit 1 basic difficult

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,98 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Employee[] employeeList = new Employee[10];
+        Employee ivanov = new Employee("ivanov", "ivan", "ivanovich", 1, 10000);
+        employeeList[0] = ivanov;
+        Employee petrov = new Employee("petrov", "petr", "petrovich", 2, 15000);
+        employeeList[2] = petrov;
+        Employee sergeev = new Employee("sergeev", "sergey", "sergeevich", 3, 50000);
+        employeeList[8] = sergeev;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        AllEmployees(employeeList);
+        System.out.println(MonthSalary(employeeList));
+        System.out.println(EmployeeWithMinSalary(employeeList));
+        sergeev.setFirstName("sergei");
+        System.out.println(EmployeeWithMaxSalary(employeeList));
+        System.out.println(AverageSalary(employeeList));
+        FullNameOfAllEmployees(employeeList);
+
+    }
+
+    // a
+    public static void AllEmployees(Employee[] choseList) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            System.out.println(choseList[i]);
+        }
+    }
+
+    // b
+    public static int MonthSalary(Employee[] choseList) {
+        int sum = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            sum += choseList[i].getSalary();
+        }
+        return sum;
+    }
+
+    // c
+    public static Employee EmployeeWithMinSalary(Employee[] choseList) {
+        int minSalary = choseList[0].getSalary();
+        int id = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getSalary() < minSalary) {
+                minSalary = choseList[i].getSalary();
+                id = i;
+            }
+        }
+        return choseList[id];
+    }
+
+    // d
+    public static Employee EmployeeWithMaxSalary(Employee[] choseList) {
+        int maxSalary = choseList[0].getSalary();
+        int id = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getSalary() > maxSalary) {
+                maxSalary = choseList[i].getSalary();
+                id = i;
+            }
+        }
+        return choseList[id];
+    }
+
+    // e
+    public static int AverageSalary(Employee[] choseList) {
+        int a = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            } else {
+                a++;
+            }
+        }
+        return MonthSalary(choseList) / a;
+    }
+
+    // f
+    public static void FullNameOfAllEmployees(Employee[] choseList) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            System.out.println(choseList[i].getFullName());
         }
     }
 }
