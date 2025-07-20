e1f13acdc6d14e5c0c7062e5caacc6a74734c00a
CooDashWeelley <iwan.kudashow@gmail.com>
2025-07-21 02:04:25 +0300

    edit 2 average difficult

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 public class Main {
     public static void main(String[] args) {
         Employee[] employeeList = new Employee[10];
-        Employee ivanov = new Employee("ivanov", "ivan", "ivanovich", 1, 10000);
+        Employee ivanov = new Employee("ivanov", "ivan", "ivanovich", 1, 40000);
         employeeList[0] = ivanov;
+        Employee antonov = new Employee("antonov", "anton", "antonovich", 5, 20000);
+        employeeList[1] = antonov;
         Employee petrov = new Employee("petrov", "petr", "petrovich", 2, 15000);
         employeeList[2] = petrov;
-        Employee sergeev = new Employee("sergeev", "sergey", "sergeevich", 3, 50000);
+        Employee nikitin = new Employee("nikitin", "nikita", "nikitich", 2, 21000);
+        employeeList[6] = nikitin;
+        Employee sergeev = new Employee("sergeev", "sergey", "sergeevich", 2, 50000);
         employeeList[8] = sergeev;
 
 
-        AllEmployees(employeeList);
-        System.out.println(MonthSalary(employeeList));
-        System.out.println(EmployeeWithMinSalary(employeeList));
+        allEmployees(employeeList);
+        System.out.println(monthSalary(employeeList));
+        System.out.println(employeeWithMinSalary(employeeList));
         sergeev.setFirstName("sergei");
-        System.out.println(EmployeeWithMaxSalary(employeeList));
-        System.out.println(AverageSalary(employeeList));
-        FullNameOfAllEmployees(employeeList);
+        System.out.println(employeeWithMaxSalary(employeeList));
+        System.out.println(averageSalary(employeeList));
+        fullNameOfAllEmployees(employeeList);
+        indexSalary(employeeList, 5);
+        allEmployees(employeeList);
+        System.out.println(2);
+        System.out.println(minSalaryInDepartment(employeeList, 2));
+        System.out.println(maxSalaryInDepartment(employeeList, 2));
+        System.out.println(monthSalaryInDepartment(employeeList, 2));
+        System.out.println(averageSalaryInDepartment(employeeList, 2));
+        employeesInDepartment(employeeList, 2);
+        indexSalaryInDepartment(employeeList, 2, 5);
+        employeesInDepartment(employeeList, 2);
+        salaryLessThan(employeeList, 25000);
+        salaryMoreThan(employeeList, 25000);
+
 
     }
 
     // a
-    public static void AllEmployees(Employee[] choseList) {
+    public static void allEmployees(Employee[] choseList) {
         for (int i = 0; i < choseList.length; i++) {
             if (choseList[i] == null) {
                 continue;
@@ -30,7 +47,7 @@ public class Main {
     }
 
     // b
-    public static int MonthSalary(Employee[] choseList) {
+    public static int monthSalary(Employee[] choseList) {
         int sum = 0;
         for (int i = 0; i < choseList.length; i++) {
             if (choseList[i] == null) {
@@ -42,23 +59,23 @@ public class Main {
     }
 
     // c
-    public static Employee EmployeeWithMinSalary(Employee[] choseList) {
+    public static Employee employeeWithMinSalary(Employee[] choseList) {
         int minSalary = choseList[0].getSalary();
-        int id = 0;
+        int idOfMinSalary = 0;
         for (int i = 0; i < choseList.length; i++) {
             if (choseList[i] == null) {
                 continue;
             }
             if (choseList[i].getSalary() < minSalary) {
                 minSalary = choseList[i].getSalary();
-                id = i;
+                idOfMinSalary = i;
             }
         }
-        return choseList[id];
+        return choseList[idOfMinSalary];
     }
 
     // d
-    public static Employee EmployeeWithMaxSalary(Employee[] choseList) {
+    public static Employee employeeWithMaxSalary(Employee[] choseList) {
         int maxSalary = choseList[0].getSalary();
         int id = 0;
         for (int i = 0; i < choseList.length; i++) {
@@ -74,25 +91,140 @@ public class Main {
     }
 
     // e
-    public static int AverageSalary(Employee[] choseList) {
-        int a = 0;
+    public static int averageSalary(Employee[] choseList) {
+        int amountOfEmployee = 0;
         for (int i = 0; i < choseList.length; i++) {
             if (choseList[i] == null) {
                 continue;
             } else {
-                a++;
+                amountOfEmployee++;
             }
         }
-        return MonthSalary(choseList) / a;
+        return monthSalary(choseList) / amountOfEmployee;
     }
 
     // f
-    public static void FullNameOfAllEmployees(Employee[] choseList) {
+    public static void fullNameOfAllEmployees(Employee[] choseList) {
         for (int i = 0; i < choseList.length; i++) {
             if (choseList[i] == null) {
                 continue;
             }
             System.out.println(choseList[i].getFullName());
+        }
+    }
+
+    public static void indexSalary(Employee[] choseList, int index) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            choseList[i].setSalary(choseList[i].getSalary() + choseList[i].getSalary() / 100 * index);
+        }
+    }
+
+    public static Employee minSalaryInDepartment(Employee[] choseList, int department) {
+        int minSalaryInDepartment = 0;
+        int idOfMinSalaryInDepartment = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department && minSalaryInDepartment == 0) {
+                minSalaryInDepartment = choseList[i].getSalary();
+            }
+            if (choseList[i].getDepartment() == department && minSalaryInDepartment >= choseList[i].getSalary()) {
+                minSalaryInDepartment = choseList[i].getSalary();
+                idOfMinSalaryInDepartment = i;
+            }
+        }
+        return choseList[idOfMinSalaryInDepartment];
+    }
+
+    public static Employee maxSalaryInDepartment(Employee[] choseList, int department) {
+        int maxSalaryInDepartment = 0;
+        int idOfMinSalaryInDepartment = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department && maxSalaryInDepartment == 0) {
+                maxSalaryInDepartment = choseList[i].getSalary();
+            }
+            if (choseList[i].getDepartment() == department && maxSalaryInDepartment <= choseList[i].getSalary()) {
+                maxSalaryInDepartment = choseList[i].getSalary();
+                idOfMinSalaryInDepartment = i;
+            }
+        }
+        return choseList[idOfMinSalaryInDepartment];
+    }
+
+    public static int monthSalaryInDepartment(Employee[] choseList, int department) {
+        int sum = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department) {
+                sum += choseList[i].getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static int averageSalaryInDepartment(Employee[] choseList, int department) {
+        int amountOfEmployeeInDepartment = 0;
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department) {
+                amountOfEmployeeInDepartment++;
+            }
+        }
+        return monthSalaryInDepartment(choseList, department) / amountOfEmployeeInDepartment;
+    }
+
+    public static void indexSalaryInDepartment(Employee[] choseList, int department, int index) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department) {
+                choseList[i].setSalary(choseList[i].getSalary() + choseList[i].getSalary() / 100 * index);
+            }
+        }
+    }
+
+    public static void employeesInDepartment(Employee[] choseList, int department) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (choseList[i].getDepartment() == department) {
+                System.out.println(choseList[i].getFullName() + ", " + choseList[i].getSalary());
+            }
+        }
+    }
+
+    public static void salaryLessThan (Employee[] choseList, int amount) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (amount > choseList[i].getSalary()) {
+                System.out.println(choseList[i].getId() + ", " + choseList[i].getFullName() + ", " + choseList[i].getSalary());
+            }
+        }
+    }
+
+    public static void salaryMoreThan (Employee[] choseList, int amount) {
+        for (int i = 0; i < choseList.length; i++) {
+            if (choseList[i] == null) {
+                continue;
+            }
+            if (amount < choseList[i].getSalary()) {
+                System.out.println(choseList[i].getId() + ", " + choseList[i].getFullName() + ", " + choseList[i].getSalary());
+            }
         }
     }
 }
